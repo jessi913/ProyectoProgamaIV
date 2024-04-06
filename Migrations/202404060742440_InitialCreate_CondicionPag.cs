8fa7251dd630629600a5f96ed6c20a1bf7b24227
Jessica Landaverde <119987685+jessi913@users.noreply.github.com>
2024-04-06 01:43:57 -0600

    migrationCondicionPag

---
+++ Migrations/202404060742440_InitialCreate_CondicionPag.cs
@@ -0,0 +1,30 @@
+﻿namespace ProyectoProgramcionIV.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate_CondicionPag : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CondicionPag",
+                c => new
+                    {
+                        CondicionPagoId = c.Int(nullable: false, identity: true),
+                        Codigo = c.String(nullable: false, maxLength: 50),
+                        Descripcion = c.String(nullable: false, maxLength: 50),
+                        Estado = c.Boolean(nullable: false),
+                        Dias = c.Int(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.CondicionPagoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CondicionPag");
+        }
+    }
+}
