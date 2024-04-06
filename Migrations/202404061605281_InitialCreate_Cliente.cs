585e9bfb920aa186b2ef6d42987d078563113710
Jessica Landaverde <119987685+jessi913@users.noreply.github.com>
2024-04-06 10:07:27 -0600

    Migration _Cliente

---
+++ Migrations/202404061605281_InitialCreate_Cliente.cs
@@ -0,0 +1,40 @@
+﻿namespace ProyectoProgramcionIV.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate_Cliente : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cliente",
+                c => new
+                    {
+                        ClienteId = c.Int(nullable: false, identity: true),
+                        Codigo = c.String(nullable: false, maxLength: 15),
+                        Nombres = c.String(nullable: false, maxLength: 50),
+                        Apellidos = c.String(nullable: false, maxLength: 50),
+                        GrupoDescuentoId = c.Int(nullable: false),
+                        CondicionPagoId = c.Int(nullable: false),
+                        Estado = c.Boolean(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ClienteId)
+                .ForeignKey("dbo.CondicionPag", t => t.CondicionPagoId)
+                .ForeignKey("dbo.GrupoDescuento", t => t.GrupoDescuentoId)
+                .Index(t => t.GrupoDescuentoId)
+                .Index(t => t.CondicionPagoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cliente", "GrupoDescuentoId", "dbo.GrupoDescuento");
+            DropForeignKey("dbo.Cliente", "CondicionPagoId", "dbo.CondicionPag");
+            DropIndex("dbo.Cliente", new[] { "CondicionPagoId" });
+            DropIndex("dbo.Cliente", new[] { "GrupoDescuentoId" });
+            DropTable("dbo.Cliente");
+        }
+    }
+}
