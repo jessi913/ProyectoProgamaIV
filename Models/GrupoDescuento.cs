12e6edcd829c9bbfe89a8ffc5c568f84da015335
Jessica Landaverde <119987685+jessi913@users.noreply.github.com>
2024-04-06 01:36:44 -0600

    class GrupoDescuento

---
+++ Models/GrupoDescuento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoProgramcionIV.Models
+{
+    [Table("GrupoDescuento")]
+    public class GrupoDescuento
+    {
+        [Key]
+
+        public int GrupodescuentoId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Codigo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public bool Estado { get; set; }
+
+        [Required]
+        public int Porcentaje { get; set; }
+
+        [Required]
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
