a820667ae71169d4337bcf35cbec6e7048da5be2
Jessica Landaverde <119987685+jessi913@users.noreply.github.com>
2024-04-06 01:33:48 -0600

    MigrationUnidadMedida

---
+++ Migrations/202404060732446_InitialCreate_UnidadMedida.cs
@@ -0,0 +1,29 @@
+﻿namespace ProyectoProgramcionIV.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate_UnidadMedida : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UnidadMedida",
+                c => new
+                    {
+                        UnidadmedidaId = c.Int(nullable: false, identity: true),
+                        Codigo = c.String(nullable: false, maxLength: 50),
+                        Descripcion = c.String(nullable: false, maxLength: 50),
+                        Estado = c.Boolean(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.UnidadmedidaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UnidadMedida");
+        }
+    }
+}
